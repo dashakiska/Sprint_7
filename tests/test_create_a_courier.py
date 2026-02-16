b566bbdb580446f1657148f899b20543f0b306b1
Dasha_Menshchikova <dashakuritsyna@gmail.com>
2026-02-16 22:10:46 +0500

    classes and allure step added

--- tests/test_create_a_courier.py
+++ tests/test_create_a_courier.py
@@ -6,52 +6,59 @@ from helpers import register_new_courier_and_return_login_password
 from url import URL
 from generators import generate_fake_data
 
+class TestCreateCourier:
+    @allure.title("Нельзя создать двух одинаковых курьеров")
+    def test_create_two_identical_courier_fail(self):
+        first_courier = register_new_courier_and_return_login_password()
+        len(first_courier) == 3
+        payload = {
+            "login": first_courier[0],
+            "password": first_courier[1],
+            "firstName": first_courier[2]
+        }
+        with allure.step("Отправка POST запроса на создание курьера"):
+            response = requests.post(URL.CREATE_COURIER_ENDPOINT, data=payload)
 
-@allure.title("Курьера можно создать")
-def test_create_courier_success():
-    new_courier = register_new_courier_and_return_login_password()
-    assert len(new_courier) == 3
+        with allure.step("ОПолучение в ответе кода 409"):    
+            assert response.status_code == 409
 
+        with allure.step("Получение сообщения об ошибке"):
+            assert response.json() == {"message": "Этот логин уже используется"}
 
-@allure.title("Нельзя создать двух одинаковых курьеров")
-def test_create_two_identical_courier_fail():
-    first_courier = register_new_courier_and_return_login_password()
-    len(first_courier) == 3
-    payload = {
-        "login": first_courier[0],
-        "password": first_courier[1],
-        "firstName": first_courier[2]
-    }
-    response = requests.post(URL.CREATE_COURIER_ENDPOINT, data=payload)
-    assert response.status_code == 409
-    assert response.json() == {"message": "Этот логин уже используется"}
+    @allure.title("Создание курьера без обязательного поля логин")
+    def test_create_courier_without_login_fail(self):
+        data = generate_fake_data()
+        payload = {
+            "password": data["password"],
+            "firstName": data["firstName"]
+        }
 
-@allure.title("Создание курьера без обязательного поля логин")
-def test_create_courier_without_login_fail():
-    data = generate_fake_data()
-    payload = {
-        "password": data["password"],
-        "firstName": data["firstName"]
-    }
+        with allure.step("Отправка POST запроса на создание курьера"):
+            response = requests.post(URL.CREATE_COURIER_ENDPOINT, data=payload)
 
-    response = requests.post(URL.CREATE_COURIER_ENDPOINT, data=payload)
+        with allure.step("ОПолучение в ответе кода 400"):
+            assert response.status_code == 400
 
-    assert response.status_code == 400
-    assert response.json() == {"message": "Недостаточно данных для создания учетной записи"}
+        with allure.step("Получение сообщения об ошибке"):    
+            assert response.json() == {"message": "Недостаточно данных для создания учетной записи"}
 
-@allure.title("Создание курьера. успешный запрос возвращает ok:true") 
-def test_create_courier_status_true():
-    data = generate_fake_data()
-    payload = {
-        "login": data["login"],
-        "password": data["password"],
-        "firstName": data["firstName"]
-    }  
+    @allure.title("Создание курьера. успешный запрос возвращает ok:true") 
+    def test_create_courier_status_true(self):
+        data = generate_fake_data()
+        payload = {
+            "login": data["login"],
+            "password": data["password"],
+            "firstName": data["firstName"]
+        } 
 
-    response = requests.post(URL.CREATE_COURIER_ENDPOINT, data=payload)
+        with allure.step("Отправка POST запроса на создание курьера"):
+            response = requests.post(URL.CREATE_COURIER_ENDPOINT, data=payload)
 
-    assert response.status_code == 201
-    assert response.json() == {"ok": True}
+        with allure.step("ОПолучение в ответе кода 201"):
+            assert response.status_code == 201
+
+        with allure.step("Получение в ответе ok true"):    
+            assert response.json() == {"ok": True}
       
 
 
--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -5,13 +5,19 @@ import requests
 from url import URL
 from data import DataForOrderScooter
 
-@allure.title("когда создаёшь заказ можно указать один из цветов — BLACK или GREY, оба, не указывать цвет")
-@pytest.mark.parametrize("color", [["BLACK"], ["GREY"], ["BLACK", "GREY"], []])
-def test_create_order_color_scooter(color):
-  data_col = DataForOrderScooter.ORDER_SCOOTER_DATA.copy()
-  data_col["color"] = color
+class TestCreateOrder:
+    @allure.title("когда создаёшь заказ можно указать один из цветов — BLACK или GREY, оба, не указывать цвет")
+    @pytest.mark.parametrize("color", [["BLACK"], ["GREY"], ["BLACK", "GREY"], []])
+    
+    def test_create_order_color_scooter(self, color):
+        data_col = DataForOrderScooter.ORDER_SCOOTER_DATA.copy()
+        data_col["color"] = color
 
-  response = requests.post(URL.CREATE_ORDER_ENDPOINT, json=data_col)
+        with allure.step("Отправка POST запроса на выбор цвета самоката"):
+            response = requests.post(URL.CREATE_ORDER_ENDPOINT, json=data_col)
 
-  assert response.status_code == 201
-  assert "track" in response.json() 
+        with allure.step("Получение в ответе кода 201"):
+            assert response.status_code == 201
+
+        with allure.step("Получение в ответе track"):
+            assert "track" in response.json() 
